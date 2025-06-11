b549124bfafe0c34f43d007d4717a7a553454948
Jonas Konrad <jonas.konrad@oracle.com>
2025-06-11 09:29:36 +0200

    Fix two buffer leaks (#11848)
    One from OSS-Fuzz, one from manual debugging. Latter has no test case because it's apparently a race condition.

--- http-server-netty/src/main/java/io/micronaut/http/server/netty/handler/MultiplexedServerHandler.java
+++ http-server-netty/src/main/java/io/micronaut/http/server/netty/handler/MultiplexedServerHandler.java
@@ -238,8 +238,8 @@ abstract class MultiplexedServerHandler {
         @Override
         public void write(@NonNull HttpResponse response, @NonNull ByteBody body) {
             if (responseDone) {
-                // early check
-                throw new IllegalStateException("Response already written");
+                // stream reset
+                return;
             }
 
             // we do some preparation immediately on the calling thread, so that the ByteBody
@@ -350,8 +350,9 @@ abstract class MultiplexedServerHandler {
 
         private void writeFull(@NonNull HttpResponse response, @NonNull ByteBuf content) {
             if (responseDone) {
-                // early check
-                throw new IllegalStateException("Response already written");
+                // stream closed
+                content.release();
+                return;
             }
             if (!ctx.executor().inEventLoop()) {
                 ByteBuf finalContent = content;
