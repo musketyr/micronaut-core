ef50412ef9babebb47b272d04b8688a8d90e4c94
Sergio del Amo <sergio.delamo@softamo.com>
2025-10-29 15:23:33 +0100

    JavaVersion.VERSION_21 instead of JavaVersion.VERSION_17

--- inject-kotlin/build.gradle.kts
+++ inject-kotlin/build.gradle.kts
@@ -83,8 +83,8 @@ tasks {
     }
 }
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 kotlin {
     compilerOptions {
